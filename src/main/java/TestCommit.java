d45aaafabcd4ecdd37a8b8de24043e6d2fabde72
kaixiuren <kaixiuren@tencent.com>
2022-06-10 13:02:47 +0800

    增加代码

--- src/main/java/TestCommit.java
+++ src/main/java/TestCommit.java
@@ -8,5 +8,6 @@ public class TestCommit {
     public void testCommit() {
         System.out.println("testCommit");
         System.out.println("testCommit");
+        System.out.println("testCommit");
     }
 }
